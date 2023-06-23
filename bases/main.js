47e96edb12ef5b328cc04f947012307d7fb26779
Kevin Sarria <reysabueso256@gmail.com>
2023-06-23 12:12:06 -0500

    avances

--- bases/main.js
+++ bases/main.js
@@ -1,18 +1,2 @@
 "use strict";
-(() => {
-    const avengers = {
-        nick: 'Samuel L. Jackson',
-        ironman: 'Robert Downey Jr.',
-        vision: 'Paul Bettany',
-        activo: true,
-        poder: 1500
-    };
-    const { poder, vision } = avengers;
-    console.log(poder, vision.toUpperCase());
-})();
-(() => {
-    console.log('let');
-    const nombre = 'Kevin';
-    const getName = function () { };
-})();
 //# sourceMappingURL=main.js.map
